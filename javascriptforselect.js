0de61e3871ac79ed07ee75dbe99ff35792a45ec2
Liliana Torres <lilianatorres0412@gmail.com>
2020-11-07 21:23:14 -0600

    Select country in index and show covid info in main

---
+++ javascriptforselect.js
@@ -0,0 +1,44 @@
+//to conect the api countries with the drop down
+searchItems = $("#searchItems")
+selectedCountry = null
+selectedCountryName = null
+$.ajax({url: "https://api.covid19api.com/countries", method: "get"}).then(
+    function(response){
+        response.sort(compare)
+        for (i of response){
+
+            a = $("<a>")
+            a.text(i.Country)
+            a.attr("data-slug",i.Slug)
+            a.attr("href", "#")
+            a.addClass("dropdown-item")
+            a.appendTo(searchItems)
+
+        }
+    }
+)
+
+//
+$(document).on("click",".dropdown-item",function(){
+    $("#hover-me").text($(this).text())
+    selectedCountry = $(this).attr("data-slug")
+    selectedCountryName = $(this).text()
+    console.log(selectedCountry)
+})
+
+function compare( a, b ) {
+    if ( a.Country < b.Country ){
+      return -1;
+    }
+    if ( a.Country > b.Country ){
+      return 1;
+    }
+    return 0;
+  }
+
+
+$("#searchButton").on("click",function(){
+    localStorage.setItem("CountryName", selectedCountryName)
+    localStorage.setItem("Country",selectedCountry)
+    window.location = "develop/main.html"
+})
